4ae62e2014e0e9d0423bb44bce93ecbffd581756
kaiakruus <kaiakruus@gmail.com>
2024-10-16 16:28:44 +0200

    test4

--- music_data_analyser.py
+++ music_data_analyser.py
@@ -1,2 +1,2 @@
-#hsgsjkdgh
 
+#wjqdwdboddjnsanöaj
--- spotify_api_miner.py
+++
@@ -1,4 +0,0 @@
-#Get list of tracks on a specific album
-#Itirate through the list and check each song if it's explicit
-#if it is add it to a seperate list of only the explicit songs
-#Print how many songs that are explicit, adn which ones they are 
