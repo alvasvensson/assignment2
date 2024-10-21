c009adf8d3149479fd8677336da54eafae732da0
valeroesther <valeroesther24@gmail.com>
2024-10-21 12:24:53 +0200

    test 2

--- music_data_analyser.py
+++ music_data_analyser.py
@@ -1,2 +1,19 @@
-#hsgsjkdgh
+#print options
+def print_options():
+    print(f"""
+    ******Welcome to our spotify analyzer!*****
+    
+    
+    """)
+
+#calculate distance average person could walk while listening ot album
+def calculate_walk_distance(album_duration):
+    album_seconds = album_duration * 1 #multiply to get seconds
+    average_speed = 1.4 #m/s
+    distance = album_seconds * average_speed
+
+    return distance
+
+
+
 
